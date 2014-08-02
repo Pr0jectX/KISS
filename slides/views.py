d9c55d63fe22e509ef79f2facea5d73ccdb586dc
Mathias Bøhn Grytemark <mathias@grytemark.no>
2014-08-02 21:45:37 +0200

    one slide working

--- slides/views.py
+++ slides/views.py
@@ -3,8 +3,10 @@ from django.shortcuts import render
 from KISS.common import rtr
 
 def defaultscreen (request):
-
+	from slides.models import Slide
 
 	c = {}
+
+	c['slide'] = Slide.objects.all()[0]
 
 	return rtr ("slide.html", c, request)
