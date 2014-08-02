8af0dad3747659d5449475832ac325c85fb655a7
Mathias Bøhn Grytemark <mathias@grytemark.no>
2014-08-02 20:56:50 +0200

    cleaning and renaming app

--- content/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-
-from content.models import Slide
-
-admin.site.register (Slide)
--- content/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-# Create your models here.
-
-class Slide (models.Model):
-	title = models.CharField (max_length=200)
-	created_date = models.DateTimeField('date created')
-	html = models.TextField ()
