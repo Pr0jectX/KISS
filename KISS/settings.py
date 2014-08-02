4711cee8fd03be26ca92628767948d6cc547c62c
Mathias Bøhn Grytemark <mathias@grytemark.no>
2014-08-02 22:03:30 +0200

    using advanced YUI for alignment and so on

--- KISS/settings.py
+++ KISS/settings.py
@@ -82,3 +82,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+##########
+##############
+##################
+DJANGO_WYSIWYG_FLAVOR = 'yui_advanced'
